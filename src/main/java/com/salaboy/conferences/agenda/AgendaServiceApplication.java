c601856bf111ab0f7e7982264225d4f46664fbe2
salaboy <Salaboy@gmail.com>
2022-05-30 06:31:22 +0100

    adding configuration processor

--- src/main/java/com/salaboy/conferences/agenda/AgendaServiceApplication.java
+++ src/main/java/com/salaboy/conferences/agenda/AgendaServiceApplication.java
@@ -1,9 +1,20 @@
 package com.salaboy.conferences.agenda;
 
+import io.netty.channel.socket.nio.NioDatagramChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.TypeAccess;
+import org.springframework.nativex.hint.TypeHint;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@NativeHint(trigger = NioSocketChannel.class, types = {
+        @TypeHint(types = NioSocketChannel.class, access = {TypeAccess.DECLARED_CONSTRUCTORS, TypeAccess.DECLARED_METHODS}),
+        @TypeHint(types = NioDatagramChannel.class)
+})
 public class AgendaServiceApplication {
 
     public static void main(String[] args) {
